77674ba5e67a2be3e6419183c21dc326fb1f41f5
ulichovick@manjaro <ulichovick@gmail.com>
2021-02-24 23:23:44 -0500

    minor bugfixes

--- backpropagation.py
+++ backpropagation.py
@@ -17,9 +17,9 @@ def backpropagation(AL, Y, caches, cache):
     dA_prev_temp = np.dot(cache[1].T, dZ_temp)
     dW_temp = 1/m * np.dot(dZ_temp, cache[0].T)
     db_temp = 1/m * np.sum(dZ_temp,axis=1,keepdims=True)
-    grads["dA" + str(l + 1)] = dA_prev_temp
-    grads["dW" + str(l + 1)] = dW_temp
-    grads["db" + str(l + 1)] = db_temp
+    grads["dA" + str(L)] = dA_prev_temp
+    grads["dW" + str(L)] = dW_temp
+    grads["db" + str(L)] = db_temp
 
     for l in reversed(range(L-1)):
         current_cache = caches[l]
