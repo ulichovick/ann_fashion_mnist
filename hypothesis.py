so im fucking stupid and the error was in the declaration of the network dimensions xD time to fix the backprop and add softmax to multiclass clasification

hypothesis.py
@@ -4,7 +4,7 @@ def hypothesis(A, W, b):
     """
     hypothesis 
     """
-    Z = np.dot(A,W.T)+b
+    Z = np.dot(W,A)+b
 
     cache = (A,W,b)
     return Z, cache