added one hot encoding(im fcking dumb) fixed the fw prop because i was applying softmax it post sigmoid(big mistake you gotta apply it after lineal/hypothesis) with one hot encoding the cost function works (cross entropy) and the model converges TODO: optimize data, code relu and apply mini batch gradient descent aaand optimize hyper parameters

softmax.py
@@ -5,4 +5,4 @@ def softmax(z):
     implement softmax function
     """
     AL = np.exp(z) / np.sum(np.exp(z), axis=0)
-    return AL
+    return AL, z