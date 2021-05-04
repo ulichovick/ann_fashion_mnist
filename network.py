c87a702c3e81ed26c493d0c23c6e7d2a5a2abc47
ulichovick@manjaro <ulichovick@gmail.com>
2021-05-03 23:03:29 -0500

    i hate the front end

--- network.py
+++ network.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask.templating import render_template
-from numpy_network.predict import predict as np_pred
-from tf_network.predict import predict as tf_pred
-import numpy as np
-from tensorflow import keras
+#from numpy_network.predict import predict as np_pred
+#from tf_network.predict import predict as tf_pred
+#import numpy as np
+#from tensorflow import keras
 
 app = Flask(__name__)
 
-parameters = np.load('numpy_network/parameters.npy', allow_pickle=True)[()]
+#parameters = np.load('numpy_network/parameters.npy', allow_pickle=True)[()]
 
-Y_prediction_valid = np_pred(parameters, "numpy_network/camisa.jpg")
+#Y_prediction_valid = np_pred(parameters, "numpy_network/camisa.jpg")
 
-model = keras.models.load_model("tf_network/ann_tf_fashmnist.h5")
-tf_pred(model, "tf_network/camiseta.jpg")
+#model = keras.models.load_model("tf_network/ann_tf_fashmnist.h5")
+#tf_pred(model, "tf_network/camiseta.jpg")
 
 @app.route("/")
 def welcome():
